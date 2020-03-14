68c198ba87b6fe8bf8812e5aa4382d77ea5fab7c
Jorge Rojas <jorgearojas25@gmail.com>
2020-03-13 20:14:59 -0500

    Areas services

---
+++ Services/services/3.store/areaStore.js
@@ -0,0 +1,35 @@
+const Model = require("../Models/areasModel");
+
+const AddArea = async area => {
+  area._id = null;
+  const myArea = new Model(area);
+  return await myArea.save();
+};
+
+const GetArea = async myFilter => {
+  let filter = {};
+  if (myFilter !== null) {
+    filter = myFilter;
+  }
+  const area = await Model.find(filter);
+  return area;
+};
+
+const UpdateArea = async body => {
+  const foundArea = Model.findById(body._id);
+  foundArea = body;
+  foundArea._id = body._id;
+  const updatedArea = await foundArea.save();
+  return updatedArea;
+};
+
+const DeleteArea = (_id) => {
+  return Model.findByIdAndDelete(_id);
+};
+
+module.exports = {
+  add: AddArea,
+  list: GetArea,
+  update: UpdateArea,
+  remove: DeleteArea
+};
