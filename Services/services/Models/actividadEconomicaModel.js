e79ff47e0b9d9bb3f87af17cdca76dfd818793e1
Jorge Rojas <jorgearojas25@gmail.com>
2020-03-18 09:13:36 -0500

    Services

--- Services/services/Models/actividadEconomicaModel.js
+++ Services/services/Models/actividadEconomicaModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const mySchema = new Schema({
-  NombreActividadEconomica: { type: String, required: true },
+  NombreTipoActividadEconomica: { type: String, required: true },
   Activo: { type: Boolean, default: true }
 });
 
